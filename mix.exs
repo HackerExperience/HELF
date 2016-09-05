584d43a525318eceeded1519cea82d361d4a3ed5
Christian Ferraz Lemos de Sousa <kress@hotmail.com.br>
2016-09-05 17:59:17 -0300

    Added more dependencies (cowboy and poison).

--- mix.exs
+++ mix.exs
@@ -1,4 +1,4 @@
-defmodule Helf.Mixfile do
+defmodule HELF.Mixfile do
   use Mix.Project
 
   def project do
@@ -15,7 +15,7 @@ defmodule Helf.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger, :he_broker],
-     mod: {Helf, []}]
+     mod: {HELF, []}]
   end
 
   # Dependencies can be Hex packages:
@@ -29,7 +29,9 @@ defmodule Helf.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:he_broker, git: "ssh://git@git.hackerexperience.com/diffusion/BROKER/HEBroker.git"}
+      {:he_broker, git: "ssh://git@git.hackerexperience.com/diffusion/BROKER/HEBroker.git"},
+      {:cowboy,"~> 1.0"},
+      {:poison, "~> 2.0"}
     ]
   end
 end
