d57603371bda08397637e311c2e19d5e89d9669b
Christian Ferraz Lemos de Sousa <kress@hotmail.com.br>
2016-09-06 18:20:39 -0300

    Changed `Router` to deal with message serialization.

--- lib/helf/router.ex
+++ lib/helf/router.ex
@@ -3,6 +3,8 @@ require Logger
 defmodule HELF.Router do
   @behaviour :cowboy_websocket_handler
 
+  alias HELF.Router.Topics
+
   # starts this router
   def run(port \\ 8080) do
     Logger.info "Router is listening at #{port}."
@@ -31,7 +33,11 @@ defmodule HELF.Router do
 
   # json message handler
   def websocket_handle({:text, message}, req, state) do
-    {:reply, {:text, "pong"}, req, state} # TODO: use the message
+    res = route(message)
+      |> format_reply
+      |> Poison.encode!
+
+    {:reply, {:text, res}, req, state}
   end
 
   # format and forward elixir messages
@@ -44,4 +50,21 @@ defmodule HELF.Router do
     :ok # TODO: match termination reason
   end
 
+  defp route(msg) do
+    case Poison.decode(msg, as: %Topics.Request{}) do
+      {:ok, %{topic: topic, args: args}} ->
+        Topics.handle_route(topic, args)
+      _ ->
+        {:error, {400, "SyntaxError"}}
+    end
+  end
+
+  defp format_reply(reply) do
+    case reply do
+      {:ok, res} ->
+        %{code: 200, data: res}
+      {:error, {code, msg}} ->
+        %{code: code, data: msg}
+    end
+  end
 end
