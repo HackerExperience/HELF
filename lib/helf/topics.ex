867d3327ed5254cb7d544636c7f0cc750de394e3
Christian Ferraz Lemos de Sousa <kress@hotmail.com.br>
2016-09-06 18:19:15 -0300

    Changed `Router.Topics` to just deal with message propagation.

--- lib/helf/topics.ex
+++ lib/helf/topics.ex
@@ -1,13 +1,32 @@
-defmodule HELF.Router.Topics.TopicRequest do
+defmodule HELF.Router.Topics.Request do
   @derive [Poison.Encoder]
   defstruct [:topic, :args]
 end
 
 defmodule HELF.Router.Topics do
-  import Poison.Encoder
-  alias HE.Broker
 
-  def route(msg) do
+  alias HELF.Broker
+
+  @remaps %{
+    "account.create" => "account:create",
+    "account.login" => "account:login"
+  }
+
+  def handle_route(topic, args) do
+    case Map.get(@remaps, topic) do
+      nil ->
+        do_route(topic, args)
+      remap ->
+        Broker.call(remap, args)
+    end
+  end
+
+  defp do_route("ping", _) do
+    {:ok, "pong"}
+  end
+
+  defp do_route(name, _) do
+    {:error, {404, "Route `#{name}` not found."}}
   end
 
 end
