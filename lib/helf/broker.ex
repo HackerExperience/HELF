ac20a523a979181f9cdc8215dd570b164c4cf5c8
Christian Ferraz Lemos de Sousa <kress@hotmail.com.br>
2016-09-15 19:51:47 -0300

    Added subscribe and broadcast helpers. Broadcast functionality is not valid.

--- lib/helf/broker.ex
+++ lib/helf/broker.ex
@@ -2,9 +2,12 @@ defmodule HELF.Broker do
   alias HELF.Error
   alias HeBroker.Publisher
 
+  def subscribe(app, route, fun) do
+    HeBroker.Consumer.subscribe(app, route, fun)
+  end
+
   def call(topic, args) do
     publisher = Publisher.start_link
-
     case Publisher.call(publisher, topic, args) do
       {:reply, res} -> res
        :noreply -> {:error, Error.format_reply(:noreply, 500, "Did not get a reply")}
@@ -15,4 +18,10 @@ defmodule HELF.Broker do
     Publisher.start_link
       |> Publisher.cast(topic, args)
   end
+
+  def broadcast(topic, args) do
+    # TODO: use a real HeBroker broadcast function
+    Broker.cast(topic, args)
+  end
+
 end
