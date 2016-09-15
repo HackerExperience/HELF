3f59adaad970f2a137aa8a9aa625e900a67e5ab5
Christian Ferraz Lemos de Sousa <kress@hotmail.com.br>
2016-09-15 20:46:09 -0300

    Added Topics server.

--- lib/helf.ex
+++ lib/helf.ex
@@ -2,12 +2,14 @@ defmodule HELF.App do
   use Application
 
   alias HELF.Router
+  alias HELF.Router.Topics
 
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
     children = [
       worker(HeBroker, []),
+      worker(Topics, []),
       worker(Router, [], function: :run)
     ]
 
