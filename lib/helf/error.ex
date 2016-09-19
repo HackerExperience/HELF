462de9c594820334205f2fccd210959cb4f97707
Christian Ferraz Lemos de Sousa <kress@hotmail.com.br>
2016-09-19 20:14:14 -0300

    Adding a `:bad_request` error format.

--- lib/helf/error.ex
+++ lib/helf/error.ex
@@ -6,6 +6,8 @@ defmodule HELF.Error do
 
   def format_reply(error, msg \\ "") do
     case error do
+      :bad_request ->
+        {400, msg}
       :unauthorized ->
         {401, msg}
       :not_found ->
