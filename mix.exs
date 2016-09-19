9c9f2e04acc6b23a658634eb96eb04a499f7224c
Christian Ferraz Lemos de Sousa <kress@hotmail.com.br>
2016-09-19 20:33:34 -0300

    Bumping version from `1.1.0` to `1.1.1`.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule HELF.Mixfile do
 
   def project do
     [app: :helf,
-     version: "1.1.0",
+     version: "1.1.1",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
