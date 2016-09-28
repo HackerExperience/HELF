9e33f7167c07b984f179718eed90b78002c2affe
Christian Ferraz Lemos de Sousa <kress@hotmail.com.br>
2016-09-28 14:18:09 -0300

    Updating Tester error message and adding a basic documentation.

--- lib/helf/tester.ex
+++ lib/helf/tester.ex
@@ -1,5 +1,5 @@
 defmodule HELF.Tester.CheckError do
-  defexception message: "No given reason."
+  defexception message: ""
 end
 
 defmodule HELF.Tester do
@@ -19,13 +19,13 @@ defmodule HELF.Tester do
         case msg do
           "a" -> :ok
           _ -> {:error, "Expected \"a\"."}
-        end
+        enmod
       end)
 
     Tester.broker_cast(pid, "event:name", "a") # -> should pass
     Tester.broker_cast(pid, "event:name", "b") # -> should fail and crash the server
 
-  Things TODO: Check if there is better ways to crash the server
+    Things TODO: Check if there is better ways to crash the server
   """
 
   alias HELF.Tester
@@ -34,79 +34,86 @@ defmodule HELF.Tester do
   defstruct(name: nil)
 
   @doc ~S"""
+  Registers a call checker.
   """
   def register(pid, topic, call: checker) do
     GenServer.call(pid, {:add_call, topic, checker})
   end
 
   @doc ~S"""
+  Registers a cast checker
   """
   def register(pid, topic, cast: checker) do
     GenServer.call(pid, {:add_cast, topic, checker})
   end
 
   @doc ~S"""
+  Makes a broker call from the Tester process.
   """
   def broker_call(pid, topic, args, timeout \\ 5000) do
     GenServer.cast(pid, {:broker_call, topic, args, timeout})
   end
 
   @doc ~S"""
+  Makes a broker cast from the Tester process.
   """
   def broker_cast(pid, topic, args) do
     GenServer.cast(pid, {:broker_cast, topic, args})
   end
 
-  # Checks
-  defp check(checker, args, state) do
+  # Calls the checker, throws an error with tester name and topic
+  defp check(checker, args, topic, state) do
     case checker.(args) do
       :ok -> :ok
       :error ->
         raise(Tester.CheckError,
-          message: "Tester '#{state.name}' failed with no explicit reason.")
+          message: "Tester '#{state.name}/#{topic}' failed with no explicit reason.")
       {:error, msg} ->
         raise(Tester.CheckError,
-          message: "Tester '#{state.name}' failed:\n  #{msg}")
+          message: "Tester '#{state.name}/#{topic}' failed:\n  #{msg}")
     end
   end
 
-  # callbacks
-
   @doc ~S"""
+  Starts the server, takes a name parameter.
   """
   def start_link(name) do
     GenServer.start_link(__MODULE__, name)
   end
 
   @doc ~S"""
+  Initializes the server with a name parameter.
   """
   def init(name) do
     {:ok, %__MODULE__{name: name}}
   end
 
   @doc ~S"""
+  Adds a call checker to the state.
   """
   def handle_call({:add_call, topic, checker}, _from, state) do
     Broker.subscribe(state.name, topic, call:
-      fn pid,_,args,timeout ->
-        check(checker, args, state)
+      fn _,_,args,_ ->
+        check(checker, args, topic,  state)
       end)
 
     {:reply, :ok, state}
   end
 
   @doc ~S"""
+  Adds a cast checker to the state.
   """
   def handle_call({:add_cast, topic, checker}, _from, state) do
     Broker.subscribe(state.name, topic, cast:
-      fn pid,_,args ->
-        check(checker, args, state)
+      fn _,_,args ->
+        check(checker, args, topic, state)
       end)
 
     {:reply, :ok, state}
   end
 
   @doc ~S"""
+  Makes an async Broker call.
   """
   def handle_cast({:broker_call, topic, args, timeout}, state) do
     Task.async(fn -> Broker.call(topic, args, timeout) end)
@@ -114,6 +121,7 @@ defmodule HELF.Tester do
   end
 
   @doc ~S"""
+  Makes a Broker cast.
   """
   def handle_cast({:broker_cast, topic, args}, state) do
     Broker.cast(topic, args)
