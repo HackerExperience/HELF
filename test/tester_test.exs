1e9111a56e22a46c1b1342fad0f85cb17f82a38b
Christian Ferraz Lemos de Sousa <kress@hotmail.com.br>
2016-10-04 19:29:36 -0300

    Updating Tester tests.

--- test/tester_test.exs
+++ test/tester_test.exs
@@ -7,70 +7,85 @@ defmodule HELF.TesterTest do
 
   setup do
     {:ok, _} = Application.ensure_all_started(:helf)
-    {:ok, pid} = Tester.start_link(self())
-    {:ok, pid: pid, service: :tester_tests}
+    :ok
   end
 
-  test "broker cast", %{pid: pid, service: service} do
+  test "broker cast" do
+    service = :test01
+    {:ok, pid} = Tester.start_link(service, self())
+
     Tester.listen(pid, :cast, service, "account:create")
 
     Tester.notify(pid, :cast, "account:create", %{username: "foo"})
-    assert_receive {:cast, "account:create"}
+    assert_receive {:cast, service, "account:create"}
     assert {:ok, %{username: "foo"}} = Tester.assert(pid, :cast, "account:create")
   end
 
-  test "sequential broker casts", %{pid: pid, service: service} do
+  test "sequential broker casts" do
+    service = :test02
+    {:ok, pid} = Tester.start_link(service, self())
+
     Tester.listen(pid, :cast, service, "account:create")
 
     Tester.notify(pid, :cast, "account:create", %{username: "foo"})
-    assert_receive {:cast, "account:create"}
+    assert_receive {:cast, service, "account:create"}
     assert {:ok, %{username: "foo"}} = Tester.assert(pid, :cast, "account:create")
 
     Tester.notify(pid, :cast, "account:create", %{username: "bar"})
-    assert_receive {:cast, "account:create"}
+    assert_receive {:cast, service, "account:create"}
     assert {:ok, %{username: "bar"}} = Tester.assert(pid, :cast, "account:create")
   end
 
-  test "broker call", %{pid: pid, service: service} do
+  test "broker call" do
+    service = :test02
+    {:ok, pid} = Tester.start_link(service, self())
+
     Tester.listen(pid, :call, service, "account:create")
 
     Tester.notify(pid, :call, "account:create", %{username: "bar"})
-    assert_receive {:call, "account:create"}
+
+    assert_receive {:call, service, "account:create"}
     assert {:ok, %{username: "bar"}} = Tester.assert(pid, :call, "account:create")
   end
 
-  test "sequential broker calls", %{pid: pid, service: service} do
+  test "sequential broker calls" do
+    service = :test03
+    {:ok, pid} = Tester.start_link(service, self())
+
     Tester.listen(pid, :call, service, "account:create")
 
     Tester.notify(pid, :call, "account:create", %{username: "foo"})
-    assert_receive {:call, "account:create"}
+    assert_receive {:call, service, "account:create"}
     assert {:ok, %{username: "foo"}} = Tester.assert(pid, :call, "account:create")
 
     Tester.notify(pid, :call, "account:create", %{username: "bar"})
-    assert_receive {:call, "account:create"}
+    assert_receive {:call, service, "account:create"}
     assert {:ok, %{username: "bar"}} = Tester.assert(pid, :call, "account:create")
   end
 
 
-  test "mixed broker calls and casts", %{pid: pid, service: service} do
+  test "mixed broker calls and casts" do
+    service = :test03
+    {:ok, pid} = Tester.start_link(service, self())
+
     Tester.listen(pid, :call, service, "account:create")
     Tester.listen(pid, :cast, service, "account:created")
     Tester.notify(pid, :call, "account:create", %{username: "foo"})
     Tester.notify(pid, :cast, "account:created", %{username: "bar"})
 
-    assert_receive {:call, "account:create"}
+    assert_receive {:call, service, "account:create"}
     assert {:ok, %{username: "foo"}} = Tester.assert(pid, :call, "account:create")
 
-    assert_receive {:cast, "account:created"}
+    assert_receive {:cast, service, "account:created"}
     assert {:ok, %{username: "bar"}} = Tester.assert(pid, :cast, "account:created")
 
     Tester.notify(pid, :call, "account:create", %{username: "b4z"})
     Tester.notify(pid, :cast, "account:created", %{username: "baz"})
 
-    assert_receive {:cast, "account:created"}
+    assert_receive {:cast, service, "account:created"}
     assert {:ok, %{username: "baz"}} = Tester.assert(pid, :cast, "account:created")
 
-    assert_receive {:call, "account:create"}
+    assert_receive {:call, service, "account:create"}
     assert {:ok, %{username: "b4z"}} = Tester.assert(pid, :call, "account:create")
   end
 end
