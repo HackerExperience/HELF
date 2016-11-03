8ad2cbcc6af5b52a05dbfd7e6195000287fc3800
Christian Ferraz Lemos de Sousa <kress@hotmail.com.br>
2016-11-03 21:23:07 -0200

    Optimize Mailer code.

--- config/config.exs
+++ config/config.exs
@@ -1,13 +1,7 @@
 use Mix.Config
 
 config :helf,
-  router_port: 8080,
-  mailers: [HELF.MailgunMailer]
-
-config :helf, HELF.MailgunMailer,
-  adapter: Bamboo.MailgunAdapter,
-  api_key: System.get_env("HELF_MAILER_MAILGUN_API"),
-  domain: System.get_env("HELF_MAILER_MAILGUN_DOMAIN")
+  router_port: 8080
 
 if Mix.env === :dev do
   config :remix,
--- lib/helf/mailer.ex
+++ lib/helf/mailer.ex
@@ -1,26 +1,52 @@
 defmodule HELF.Mailer do
-
   alias Bamboo.Email, as: BEmail
 
-  def email(params \\ []) do
-    mailers = Application.fetch_env!(:helf, :mailers)
-    default_sender = Application.fetch_env!(:helf, :default_sender)
+  @mailers Application.fetch_env!(:helf, :mailers)
+  @default_author Application.fetch_env!(:helf, :default_author)
 
-    payload = BEmail.new_email(
-      from: Keyword.get(params, :from, default_sender),
-      to: Keyword.fetch!(params, :to),
-      subject: Keyword.fetch!(params, :subject),
-      html_body: Keyword.fetch!(params, :html),
-      text_body: Keyword.fetch!(params, :text)
-    )
+  def send_email(params \\ []) do
+    author =
+      Keyword.get(params, :from, @default_author)
+      |> format_entity()
 
-    Enum.reduce_while(mailers, :error, fn (mailer, _) ->
-      try do
-        mailer.deliver_now(payload)
-        {:halt, {:ok, mailer}}
-      rescue
-        Bamboo.MailgunAdapter.ApiError -> {:cont, :error}
-      end
+    target =
+      Keyword.fetch!(params, :to)
+      |> format_entity()
+
+    subject = Keyword.fetch!(params, :subject)
+    html = Keyword.fetch!(params, :html)
+    text = Keyword.fetch!(params, :text, "")
+
+    BEmail.new_email()
+    |> BEmail.from(author)
+    |> BEmail.to(target)
+    |> BEmail.subject(subject)
+    |> BEmail.html_body(html)
+    |> BEmail.text_body(text)
+    |> do_send()
+  end
+
+  defp do_send(email) do
+    Enum.reduce_while(@mailer, :error, fn (mailer, _) ->
+      do_send(mailer, email)
     end)
   end
+
+  defp do_send(mailer, email) do
+    try do
+      mailer.deliver_now(payload)
+      {:halt, {:ok, mailer}}
+    rescue
+      Bamboo.MailgunAdapter.ApiError -> {:cont, :error}
+      Bamboo.MandrillAdapter.ApiError -> {:cont, :error}
+      Bamboo.SendgridAdapter.ApiError -> {:cont, :error}
+      Bamboo.SentEmail.DeliveriesError -> {:cont, :error}
+      Bamboo.SentEmail.NoDeliveriesError -> {:cont, :error}
+    end
+  end
+
+  defp format_entity({name, email}),
+    do: "#{name} <#{email}>"
+  defp format_entity(email),
+    do: email
 end
