de76aa77df37355806a563a631093ebd78b43659
mememori <charlotte.elixir@gmail.com>
2017-03-17 20:14:37 -0300

    Remove remix from deps

--- mix.exs
+++ mix.exs
@@ -6,27 +6,20 @@ defmodule HELF.Mixfile do
       app: :helf,
       version: "0.0.1",
       elixir: "~> 1.3",
-      build_embedded: Mix.env == :prod,
-      start_permanent: Mix.env == :prod,
       deps: deps()]
   end
 
   def application do
     [
-      applications: applications(Mix.env),
       mod: {HELF.App, []}]
   end
-
-  defp applications(:dev), do: default_applications() ++ [:remix]
-  defp applications(_), do: default_applications()
-  defp default_applications, do: [:logger, :hebroker, :cowboy, :bamboo]
 
   defp deps do
     [
       {:cowboy,"~> 1.0"},
       {:poison, "~> 2.0"},
       {:bamboo, "~> 0.7"},
-      {:hebroker, git: "ssh://git@git.hackerexperience.com/diffusion/BROKER/HEBroker.git", ref: "v0.1"},
-      {:remix, "~> 0.0.1", only: :dev}]
+      {:hebroker, git: "ssh://git@git.hackerexperience.com/diffusion/BROKER/HEBroker.git", ref: "v0.1"}
+    ]
   end
 end
