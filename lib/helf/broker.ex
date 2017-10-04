27892d3dd0729d667f5848869d35844d741a056d
Renato Massaro <renato@hackerexperience.com>
2017-10-04 02:20:39 -0300

    Remove Broker/Router

--- lib/helf/broker.ex
+++
@@ -1,3 +0,0 @@
-defmodule HELF.Broker do
-  use HeBroker.GenBroker
-end
--- lib/helf/router/request.ex
+++
@@ -1,4 +0,0 @@
-defmodule HELF.Router.Request do
-  @derive [Poison.Encoder]
-  defstruct [:topic, :args, :request_id, debug: false]
-end
