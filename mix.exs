ddc25fb713b04e3b5be89b9fb6b2c13abb66ccc0
Renato Massaro <renato@hackerexperience.com>
2018-02-27 21:46:51 -0300

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule HELF.Mixfile do
   def project do
     [
       app: :helf,
-      version: "0.0.2",
+      version: "0.0.3",
       elixir: "~> 1.5",
       dialyzer: [plt_add_apps: [:mix]],
       deps: deps(),
